b23348cb10e7e4ca2f68b59ec9c6d8869e430751
Schenkel <thi.schenkel@gmail.com>
2021-03-01 15:12:57 +0100

    Exercice 1 done without table

--- chapitre_01/index.js
+++ chapitre_01/index.js
@@ -3,4 +3,30 @@ const express = require('express');
 const app = express();
 app.listen(8000, () => {
     console.log('serveur lancé');
-})
+});
+
+
+// Exercice 0
+app.get("/", (req, res) => {
+    console.log(req);
+    res.send("Authors API");
+});
+
+
+//  Exercice 1
+app.get("/authors/1", (req, res) => {
+    res.send(`Lawrence Nowell, UK `);
+});
+
+app.get("/authors/2", (req, res) => {
+    res.send(`William Shakespeare, UK `);
+});
+
+app.get("/authors/3", (req, res) => {
+    res.send(`Charles Dickens, US `);
+});
+
+app.get("/authors/4", (req, res) => {
+    res.send(`Oscar Wilde, UK `);
+});
+
