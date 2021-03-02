88da6b770ad767a6533a64be37ccf0c3c334eef0
Schenkel <thi.schenkel@gmail.com>
2021-03-02 17:10:32 +0100

    Localization translations.json done

---
+++ express_l10n/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require("express-handlebars");
+const lang = require("./translations.json");
+const app = express();
+const port = 8001;
+
+app.listen(port, () => {
+    console.log('serveur lancé');
+});
+// app.use(express.static('public'));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/:lang?', (req, res) => {
+
+    res.render('home');
+});
+
+
+
