d3d9fec3b07d066e71c0e7cff8bc8ffbbd5dc3ab
Schenkel <thi.schenkel@gmail.com>
2021-03-03 13:50:59 +0100

    Integration is beginning

--- express_integration/index.js
+++ express_integration/index.js
@@ -6,3 +6,34 @@ const port = 8001;
 app.listen(port, () => {
     console.log('serveur lancé');
 });
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/team', (req, res) => {
+    res.render('team');
+});
+
+app.get('/team-member', (req, res) => {
+    res.render('team-member');
+});
+
+app.get('/recipe', (req, res) => {
+    res.render('recipe');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
+
+app.get('/page2', (req, res) => {
+    res.render('page2');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
