28500ca63d27ebd7033998fbfe8e1cf122f64704
Schenkel <thi.schenkel@gmail.com>
2021-03-04 15:25:10 +0100

    It works with Postman

--- chapitre_03/backend/index.js
+++ chapitre_03/backend/index.js
@@ -6,4 +6,31 @@ app.listen(port, () => {
     console.log('Serveur lancé');
 });
 
+const students = [
+    {
+        name: "Princess",
+        firstName: "Peach",
+        age: 20,
+    },
+    {
+        name: "King",
+        firstName: "Bouh",
+        age: 25,
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Page accueil');
+});
+
+app.get('/students/', (req, res) => {
+    res.json(students);
+});
+
+app.post('/students', (req, res) => {
+    students.push(req.body);
+    res.send(`Le nom de l'étudiant a été ajouté`);
+})
+
+
 
