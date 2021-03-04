44b36fce60cebcd53392a1ba6ea5ba96ae5e9d1f
Schenkel <thi.schenkel@gmail.com>
2021-03-04 09:14:51 +0100

    Something is wrong

--- chapitre_02/index.js
+++ chapitre_02/index.js
@@ -3,24 +3,39 @@ const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = 8000;
 
+let userConnected = false;
+
 app.listen(port, () => {
     console.log('serveur lancé');
 });
-// app.use(express.static('public'));
 
 app.engine('handlebars', expressHandlebars());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { title: "Connection validée" });
+app.get("/", (req, res) => {
+    res.render("login", { passwordTooShort: false });
 });
 
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get("/home", (req, res) => {
+    if (userConnected) {
+        res.render("home", {
+            title: "Bonjour",
+            sousTitre: "Ca va ? "
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
-app.post('/login', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.post("/login", (req, res) => {
     console.log(req.body);
+    if (req.body.password.length < 8) {
+        res.render("login", { passwordTooShort: true });
+    } else {
+        userConnected = true;
+        res.redirect("/home");
+    }
 });
 
 
