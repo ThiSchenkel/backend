bded584dc8f358ea79da4c3e35855f6e302bb72b
Schenkel <thi.schenkel@gmail.com>
2021-03-04 09:13:59 +0100

    Translations ok

--- express_l10n/index.js
+++ express_l10n/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
-const lang = require("./translations.json");
 const app = express();
-const port = 8001;
-
-app.listen(port, () => {
-    console.log('serveur lancé');
-});
-// app.use(express.static('public'));
+const port = 8000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.listen(port, () => {
+    console.log('serveur lancé');
 });
 
-app.get('/:lang?', (req, res) => {
+const translations = {
+    fr: {
+        message: "Bonjour, ca va? ",
+        flag: "https://restcountries.eu/data/fra.svg",
+    },
+    en:
+    {
+        message: "Hello, what's up? ",
+        flag: "https://restcountries.eu/data/gbr.svg",
+    },
+    es: {
+        messsage: "Halo, como esta? ",
+        flag: "https://restcountries.eu/data/esp.svg",
+    },
+}
 
-    res.render('home');
+
+app.get('/:lang?', (req, res) => {
+    res.render('home', { lang: translations[req.params.lang ? req.params.lang : 'fr'], languages: Object.keys(translations) })
 });
