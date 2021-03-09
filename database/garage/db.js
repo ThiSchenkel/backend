9a770440b4d0235231f896c5922702f4a6d7127b
Schenkel <thi.schenkel@gmail.com>
2021-03-09 13:46:23 +0100

    Garage done

--- database/garage/db.js
+++ database/garage/db.js
@@ -4,44 +4,71 @@ mongoose.connect('mongodb://localhost:27017/garage', { useNewUrlParser: true, us
     console.log('DB connecté');
 })
 
-const CarSchema = new mongoose.Schema({
+const carSchema = new mongoose.Schema({
     marque: String,
     modele: String,
-    annee: Number
+    annee: Number,
+    created: Date
 });
 
-const CarModel = mongoose.model('cars', CarSchema);
+const carModel = mongoose.model('cars', carSchema);
 
-CarModel.deleteMany({}).then(() => {
-    CarModel.create({
-        marque: 'Renault',
-        modele: 'Espace',
-        annee: 1999
-    },
-        {
+carModel.deleteMany({}).then(() => {
+    carModel.create(
+        [{
+
             marque: 'Renault',
-            modele: 'Scenic',
-            annee: 2004
+            modele: 'Espace',
+            annee: 1999,
+            created: new Date()
         },
         {
+
+            marque: 'Renault',
+            modele: 'Scenic',
+            annee: 2004,
+            created: new Date()
+        },
+        {
+
             marque: 'Peugeot',
             modele: '308',
-            annee: 2017
-        }
-    )
-        .catch(error => console.log(error));
+            annee: 2017,
+            created: new Date()
+        }]
+    ).then((response) => console.log(`Tableau : ${response}`));
 });
 
 
-CarModel.findById('60464fa3aa1be0476427bdb8')
-    .then((response) => console.log(response));
+// Penser à mettre en commentaire le delete et create car l'id (sans être défini manuellement) est constamment créée
+carModel.findById(('60474202924bc43fe0a60090'))
+    .then((response) => console.log(`Recherche par ID : ${response}`));
 
-CarModel.updateOne(
-    {
-        _id
-            :
-            '60464fa3aa1be0476427bdb8'
-    },
-    { annee: 2000 }
-)
-    .then((response) => { console.log(response) });
+
+carModel.updateOne(
+    { _id: '60474202924bc43fe0a60090' },
+    { annee: 2000 })
+    .then((response) => { console.log(`Update : ${response}`) });
+
+
+carModel.deleteMany({
+    marque: 'Renault'
+}).then((response) => { console.log(`Efface les Renault : ${response}`) });
+
+
+carModel.insertMany([{
+    marque: 'Aston Martin',
+    modele: 'DB9',
+    annee: 2010,
+    created: new Date()
+},
+{
+    marque: 'Range Rover',
+    modele: 'Discovery Sport',
+    annee: 2017,
+    created: new Date()
+}]);
+
+carModel.find(
+    { annee: { $gt: 2015 } }
+).then((response) => { console.log(`Les voitures de + de 2015 : ${response}`) });
